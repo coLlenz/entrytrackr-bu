67869218bb398c8dd0d5cf1701354fbbe1a30666
FreeDurk <freelance.durk@gmail.com>
2021-01-02 16:11:22 +0800

    Change Wordins

--- resources/views/trakr/modal/modals.blade.php
+++ resources/views/trakr/modal/modals.blade.php
@@ -66,7 +66,7 @@
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Check Out</h5>
+            <h5 class="modal-title" id="exampleModalLabel">Sign Out</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
@@ -145,7 +145,7 @@
                 autocapitalize: 'off'
             },
             showCancelButton: true,
-            confirmButtonText: 'Check In',
+            confirmButtonText: 'Sign In',
             showLoaderOnConfirm: true,
             preConfirm: (trakrid) => {
                 if (trakrid) {
