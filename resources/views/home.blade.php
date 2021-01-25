63736199e7ba16f3119adcfd26d6401d6e8ad9de
FreeDurk <freelance.durk@gmail.com>
2021-01-25 10:12:17 +0800

    Wording Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -296,7 +296,7 @@
             title: 'This will manually sign out the visitor.',
             showCloseButton: true,
             showCancelButton: true,
-            confirmButtonText: `Procced`,
+            confirmButtonText: `Proceed`,
         }).then((result) => {
             if (result.isConfirmed) {
                 $.ajaxSetup({headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}});
