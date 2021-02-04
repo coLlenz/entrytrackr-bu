db6d87b189fea2b427935150e6e32e4893bde517
jojowebriq <jovyllebermudez@gmail.com>
2021-02-04 18:31:36 +0800

    TrakrID Crud Done

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php 
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\AdminTrakrController;
 use App\Http\Controllers\Admin\ClientAccessController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,4 +14,21 @@ Route::prefix('admin')->middleware(['auth' , 'isAdmin'])->group(function(){
 	Route::get('/client/remove/{id}' , [AdminController::class , 'removeClient'])->name('removeClient');
 	Route::get('/client/add-image/{id}' , [AdminController::class , 'uploadImageView'])->name('uploadImageView');
 	Route::post('/client/add-image/{id}' , [AdminController::class , 'uploadClientImage'])->name('uploadClientImage');
+
+	Route::get('/trakrid' , [AdminTrakrController::class , 'index'])->name('admin-trakrid');
+	Route::get('/trakrid/edit/{trakr}' , [AdminTrakrController::class , 'edit'])->name('admin-trakr-edit');
+	Route::get('/trakrid/delete/{trakr}' , [AdminTrakrController::class , 'destroy'])->name('admin-trakr-delete');
+	Route::post('/trakrid/edit/{trakr}', [AdminTrakrController::class, 'update'])->name("admin-trakr-update");
+	Route::post('/trakrid/manual/visitor/signout' , [AdminTrakrController::class , 'manualSignOut'])->name('admin-manualSignOut');
+
+	// Route::get('/', [ReportController::class, 'index'])->name("report-index");
+	// Route::get('/summary' , [ReportController::class , 'summaryReport'])->name('summaryReport');
+	
+	// Route::get('/', [ReportController::class, 'index'])->name("report-index");
+	// Route::get('/filter',[ReportController::class,'filter'])->name('report-filter');
+	// Route::get('/generate_list',[ReportController::class,'generate_pdf'])->name('list-report');
+	// Route::get('/summary' , [ReportController::class , 'summaryReport'])->name('summaryReport');
+	// Route::get('/summary/by/visitor' , [ReportController::class , 'byVisitor'])->name('byVisitor');
+	// Route::get('/summary/get/results/{question_id}/{log_id}' , [ReportController::class , 'viewResults'])->name('viewResults');
+	// Route::post('/summary/get/results/download/' , [ReportController::class , 'downloadResult'])->name('downloadResult');
 });
