2c4eec9ccd9b457a2f42ecdbab121df7a31adcf5
FreeDurk <freelance.durk@gmail.com>
2021-03-02 16:47:49 +0800

    change temp_disk to s3

--- config/excel.php
+++ config/excel.php
@@ -305,7 +305,7 @@ return [
         | in conjunction with queued imports and exports.
         |
         */
-        'remote_disk'         => null,
+        'remote_disk'         => 's3',
         'remote_prefix'       => null,
 
         /*
