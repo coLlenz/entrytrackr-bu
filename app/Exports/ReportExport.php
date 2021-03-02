4aa74ef250cace9419bb4d55fdbaf55867ada815
FreeDurk <freelance.durk@gmail.com>
2021-03-02 16:27:58 +0800

    adjust vapor changes

--- app/Exports/ReportExport.php
+++ app/Exports/ReportExport.php
@@ -5,9 +5,12 @@ use App\Models\LogReport;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 use DB;
+use Maatwebsite\Excel\Concerns\Exportable;
 
 class ReportExport implements FromView
 {
+    use Exportable;
+
     private $report_list;
 
     function __construct($report_list){
