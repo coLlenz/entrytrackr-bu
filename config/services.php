19a4f960775844f1f5d68b1380f65f729cdaf53c
FreeDurk <freelance.durk@gmail.com>
2021-03-14 14:22:58 +0800

    send email support

--- config/services.php
+++ config/services.php
@@ -27,7 +27,8 @@ return [
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+        'region' => env('AWS_DEFAULT_REGION'),
+        'version' => env('AWS_API_VERSION')
     ],
 
 ];
