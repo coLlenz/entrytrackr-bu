dde098e6d1a3912eb2c52d248f94f8cd7b4eb0dc
Durk Amaba <freelance.durk@gmail.com>
2021-04-27 10:52:35 +0800

    fix notification on trakrid signin

--- resources/views/trakr/mobile.blade.php
+++ resources/views/trakr/mobile.blade.php
@@ -8,59 +8,6 @@
      transition: 300ms
 } */
 </style>
-{{-- <div class="container boder border-light">
-    <div class="row justify-content-center">
-        <div class="col-md-12 text-center">
-            <h1 class="text-light" style="font-size: 30px;">WELCOME</h1>
-        </div>
-    </div>
-    <div class="row justify-content-center">
-        <div class="col-md-12 col-lg-4 col-sm-12 mb-4 entry_box trakr_box_hover" id="modalCheckin">
-            <a href="#" class="card" data-toggle="" data-target="">
-                <div class="card-body text-center">
-                    <h1><i class="simple-icon-login"></i><h1>
-                    <h2 class="card-text font-weight-semibold mb-0">Sign In</h2>
-                </div>
-            </a>
-        </div>
-        <div class="col-md-12 col-lg-4 col-sm-12 mb-4 entry_box trakr_box_hover" id="simple_checkin">
-            <a href="#" class="card" data-toggle="" data-target="#">
-                <div class="card-body text-center">
-                    <h1><i class="simple-icon-login"></i><h1>
-                    <h2 class="card-text font-weight-semibold mb-0">Sign In with trakrID</h2>
-                </div>
-            </a>
-        </div>
-    </div>
-    
-    <div class="row justify-content-center">
-        <div class="col-lg-8 col-sm-12 mb-4 entry_box trakr_box_hover" id="checkoutModal">
-            <a href="#" class="card" data-toggle="" id="">
-                <div class="card-body text-center">
-                    <h1><i class="simple-icon-logout"></i></h1>
-                    <h2 class="card-text font-weight-semibold mb-0">Sign Out</h2>
-                </div>
-            </a>
-        </div>
-    </div>
-    
-    <div class="row justify-content-center">
-        <div class="col-sm-8 col-md-6 col-lg-6 col-xl-4">
-            <a href="#" class="card" data-toggle="modal">
-                <div class="card-body text-center">
-                    <h1>Contactless Entry</h1>
-                    <img class=""  src="{{ $view_data['qr_path'] }}" alt="" height="250" width="250">
-                </div>
-                <div class="card-footer">
-                    <p class="text-center font-medium card-text mb-0" style="font-size: 17px;">
-                        Scan the QR code with your smartphone camera to <br>
-                        <strong>Sign In</strong> or <strong>Sign Out</strong>
-                    </p>
-                </div>
-            </a>
-        </div>
-    </div>  --}}
-    
     <div class="container-login">
         <div class="first-border-row entry_background">
             <div class="row justify-content-center">
