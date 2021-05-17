a9a80232ead1ba28cf1a0864d326155b11b15cd6
Durk Amaba <freelance.durk@gmail.com>
2021-05-17 13:06:38 +0800

    logout

--- resources/views/components/topnavbar.blade.php
+++ resources/views/components/topnavbar.blade.php
@@ -29,7 +29,7 @@
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" uk-hidden>
                     @csrf
                 </form>
-                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sign out</a>
+                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log out</a>
             </div>
         </div>
     </div>
